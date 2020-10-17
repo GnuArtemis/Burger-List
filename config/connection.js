91acb963b63a3329eb966934126c6a45e3e819ad
Ann Guinee <annmguinee@gmail.com>
2020-10-16 21:39:30 -0700

    Updated heroku mysql connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
 const mysql = require("mysql");
+let connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-})
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 
-connection.connect(function(err){
-    if(err){
-        console.error("Error connecting: " +err.stack);
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    })
+
+}
+connection.connect(function (err) {
+    if (err) {
+        console.error("Error connecting: " + err.stack);
         return;
     }
     console.log("Connected as id " + connection.threadId);
